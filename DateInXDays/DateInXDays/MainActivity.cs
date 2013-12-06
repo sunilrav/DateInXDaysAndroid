63257df4747ef61dd8291a2d500ba2c8cc173b33
sunilrav <rsunilbabu@gmail.com>
2013-12-05 17:14:20 -0700

    Initial

---
+++ DateInXDays/DateInXDays/MainActivity.cs
@@ -0,0 +1,34 @@
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DateInXDays
+{
+	[Activity (Label = "DateInXDays", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Main);
+
+			var startDatePicker = FindViewById<DatePicker> (Resource.Id.startDatePicker);
+			var numOfDaysEditText = FindViewById<EditText> (Resource.Id.numberOfDaysEditText);
+			numOfDaysEditText.Text = "10";
+
+			var resultTextView = FindViewById<TextView> (Resource.Id.resultTextView);
+			resultTextView.Text = DateTime.Now.AddDays (int.Parse(numOfDaysEditText.Text)).ToShortDateString ();
+
+			Button goButton = FindViewById<Button> (Resource.Id.goButton);
+			goButton.Click += delegate {
+				resultTextView.Text = startDatePicker.DateTime.AddDays (int.Parse(numOfDaysEditText.Text)).ToShortDateString ();
+			};
+		}
+	}
+}
+
+
