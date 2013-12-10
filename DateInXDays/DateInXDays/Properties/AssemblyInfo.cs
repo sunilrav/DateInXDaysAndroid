e72ec40b06f599b58b5730ab81a074557f6af78a
sunilrav <rsunilbabu@gmail.com>
2013-12-10 16:37:19 -0700

    publish fixes

--- DateInXDays/DateInXDays/Properties/AssemblyInfo.cs
+++ DateInXDays/DateInXDays/Properties/AssemblyInfo.cs
@@ -20,9 +20,4 @@ using Android.App;
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-#if RELEASE
-[assembly: Application(Debuggable=false)]
-#else
-[assembly: Application(Debuggable=true)]
-#endif
 
